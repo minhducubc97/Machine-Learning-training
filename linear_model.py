198b7dc1ab9b8671d5a52fb0fa48463fe3988197
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-05 12:48:04 -0700

    First upload

---
+++ linear_model.py
@@ -0,0 +1,83 @@
+##### MAKE PRETTY GRAPH
+
+# To plot pretty figures
+# %matplotlib - Not available in VSCode, only in IPython API
+
+# support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+import matplotlib
+import matplotlib.pyplot as plt
+plt.ion()
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "fundamentals"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+# Ignore useless warnings (see SciPy issue #5998)
+import warnings
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
+
+##### MERGE the OECD's life satisfaction data and the IMF's GDP per capita data
+def prepare_country_stats(oecd_bli, gdp_per_capita):
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+    gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+    gdp_per_capita.set_index("Country", inplace=True)
+    full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,
+                                  left_index=True, right_index=True)
+    full_country_stats.sort_values(by="GDP per capita", inplace=True)
+    remove_indices = [0, 1, 6, 8, 33, 34, 35]
+    keep_indices = list(set(range(36)) - set(remove_indices))
+    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+
+##### MAIN CODE
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+# Load the data
+oecd_bli = pd.read_csv("D:\Documents\W-O-R-K\SELF-LEARNING\MACHINE-LEARNING\src\datasets\lifesat\oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("D:\Documents\W-O-R-K\SELF-LEARNING\MACHINE-LEARNING\src\datasets\lifesat\gdp_per_capita.csv",thousands=',',delimiter='\t',
+                             encoding='latin1', na_values="n/a")
+
+# Prepare the data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+y = np.c_[country_stats["Life satisfaction"]]
+
+# Visualize the data
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+plt.show()
+
+# Select a linear model
+model = sklearn.linear_model.LinearRegression()
+
+# Train the model
+model.fit(X, y)
+
+# Make a prediction for Cyprus
+X_new = [[22587]]  # Cyprus' GDP per capital
+print(model.predict(X_new)) # outputs [[ 5.96242338]]]
+
+# NOTE: Run the file by typing python -i linear_model.py into the terminal
