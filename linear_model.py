562d79ac930691dbff21bf27dc576657f79ecfb0
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-05 17:04:01 -0700

    fixed syntax

--- linear_model.py
+++ linear_model.py
@@ -1,41 +1,42 @@
-##### MAKE PRETTY GRAPH
-
-# To plot pretty figures
-# %matplotlib - Not available in VSCode, only in IPython API
-
-# support both python 2 and python 3
+##### PYTHON 2 AND 3 SUPPORT
 from __future__ import division, print_function, unicode_literals
 
-# Common imports
+##### COMMON IMPORT
 import numpy as np
 import os
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.linear_model
+import warnings
 
+##### INITIALIZATION
 # to make this notebook's output stable across runs
 np.random.seed(42)
 
-import matplotlib
-import matplotlib.pyplot as plt
+# To plot pretty figures
+# %matplotlib - Not available in VSCode, only in IPython API
 plt.ion()
+
+# Specify sizes
 plt.rcParams['axes.labelsize'] = 14
 plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
 
+##### HELPER FUNCTIONS
 # Where to save the figures
-PROJECT_ROOT_DIR = "."
-CHAPTER_ID = "fundamentals"
-
-def save_fig(fig_id, tight_layout=True):
-    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format='png', dpi=300)
+# PROJECT_ROOT_DIR = "."
+# def save_fig(fig_id, tight_layout=True):
+#     path = os.path.join(PROJECT_ROOT_DIR, "graphs", fig_id + ".png")
+#     print("Saving figure", fig_id)
+#     if tight_layout:
+#         plt.tight_layout()
+#     plt.savefig(path, format='png', dpi=300)
 
 # Ignore useless warnings (see SciPy issue #5998)
-import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
-##### MERGE the OECD's life satisfaction data and the IMF's GDP per capita data
+# Merge the OECD's life satisfaction data and the IMF's GDP per capita data
 def prepare_country_stats(oecd_bli, gdp_per_capita):
     oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
     oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
@@ -49,20 +50,14 @@ def prepare_country_stats(oecd_bli, gdp_per_capita):
     return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
 
 ##### MAIN CODE
-
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import sklearn.linear_model
-
 # Load the data
-oecd_bli = pd.read_csv("D:\Documents\W-O-R-K\SELF-LEARNING\MACHINE-LEARNING\src\datasets\lifesat\oecd_bli_2015.csv", thousands=',')
-gdp_per_capita = pd.read_csv("D:\Documents\W-O-R-K\SELF-LEARNING\MACHINE-LEARNING\src\datasets\lifesat\gdp_per_capita.csv",thousands=',',delimiter='\t',
-                             encoding='latin1', na_values="n/a")
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is the Project Root
+oecd_bli = pd.read_csv(ROOT_DIR + "\datasets\lifesat\oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv(ROOT_DIR + "\datasets\lifesat\gdp_per_capita.csv", thousands=',',delimiter='\t', encoding='latin1', na_values="n/a")
 
 # Prepare the data
 country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+# np.c_ : create a 2D array
 X = np.c_[country_stats["GDP per capita"]]
 y = np.c_[country_stats["Life satisfaction"]]
 
@@ -70,7 +65,7 @@ y = np.c_[country_stats["Life satisfaction"]]
 country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
 plt.show()
 
-# Select a linear model
+# Select a linear model - Linear Regression
 model = sklearn.linear_model.LinearRegression()
 
 # Train the model
