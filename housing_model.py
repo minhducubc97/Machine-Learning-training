c1935b39d59fcc55e6e0d94b2cd115c12dc58b11
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-07 16:45:16 -0700

    start plotting

--- housing_model.py
+++ housing_model.py
@@ -12,7 +12,7 @@ import sklearn.linear_model
 import warnings
 
 ##### INITIALIZATION
-# to make this notebook's output stable across runs
+# to make the output stable across many runs (different seeds lead to different result)
 np.random.seed(42)
 
 # To plot pretty figures
@@ -26,13 +26,12 @@ plt.rcParams['ytick.labelsize'] = 12
 
 ##### COMMON HELPER FUNCTIONS
 # Where to save the figures
-# PROJECT_ROOT_DIR = "."
-# def save_fig(fig_id, tight_layout=True):
-#     path = os.path.join(PROJECT_ROOT_DIR, "graphs", fig_id + ".png")
-#     print("Saving figure", fig_id)
-#     if tight_layout:
-#         plt.tight_layout()
-#     plt.savefig(path, format='png', dpi=300)
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(ROOT_DIR, "graphs", fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
 
 # Ignore useless warnings (see SciPy issue #5998)
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
@@ -44,14 +43,13 @@ from six.moves import urllib
 import pandas as pd
 
 ##### CONSTANTS
-DOWNLOAD_LINK = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-HOUSING_PATH = "dataset/housing/"
-HOUSING_FILE = "housing.tgz"
-HOUSING_URL = DOWNLOAD_LINK + HOUSING_PATH + HOUSING_FILE
+DOWNLOAD_LINK = "https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is the Project Root
+HOUSING_PATH = ROOT_DIR + "\\datasets\\housing\\"
 
 ##### HELPER FUNCTIONS
 # Get the housing data from the Url
-def fetch_housing_data(housing_url = HOUSING_URL, housing_path = HOUSING_PATH):
+def fetch_housing_data(housing_url = DOWNLOAD_LINK, housing_path = HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
     path_to_tgz = os.path.join(housing_path, "housing.tgz")
@@ -60,11 +58,14 @@ def fetch_housing_data(housing_url = HOUSING_URL, housing_path = HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is the Project Root
 def load_housing_data(housing_path = HOUSING_PATH):
-    path_to_csv = os.path.join(ROOT_DIR, housing_path, "housing.csv")
+    path_to_csv = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(path_to_csv)
 
-# fetch_housing_data()
+#fetch_housing_data() # get the data from the given URL
 housing = load_housing_data()
-housing.head()
+#housing.head() # display the table in csv file
+#housing.describe() #show mean, average, min, ... all the values arranged
+housing.hist(bins=50, figsize=(20,15))
+save_fig("histograms")
+plt.show()
